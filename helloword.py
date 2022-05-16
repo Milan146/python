4e457c3674783a7e6a79aa46639d9589e5773c9b
Milan146 <81798808+Milan146@users.noreply.github.com>
2022-05-16 18:03:02 +0530

    os import

--- helloword.py
+++ helloword.py
@@ -1,1 +1,14 @@
+import os
+import flask
+from playsound import playsound
 print("Hello Wordl!")
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night''')
+print(os.listdir())
+
